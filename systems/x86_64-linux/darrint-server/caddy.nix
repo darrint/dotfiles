c93f818ae48ee864bcd15080b5cad22dde4d1554
darrint <darrint@fastmail.com>
2025-06-06 20:24:38 -0400

    add another default responder

--- systems/x86_64-linux/darrint-server/caddy.nix
+++ systems/x86_64-linux/darrint-server/caddy.nix
@@ -6,6 +6,9 @@
       "vw.thompsons.space".extraConfig = ''
         reverse_proxy http://localhost:8222
       '';
+      "thompsons.space".extraConfig = ''
+        respond "go away"
+      '';
       "ancientdad1974.duckdns.org".extraConfig = ''
         respond "go away"
       '';
