b265d16a53f0420a735d0c8008a02119e7954e47
darrint <darrint@fastmail.com>
2025-11-11 21:28:28 -0500

    Script actually works

--- update-with-determinate.sh
+++ update-with-determinate.sh
@@ -1,1 +1,1 @@
-nh os switch . -- --option extra-substituters https://install.determinate.systems --option extra-trusted-public-keys cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM=
+sudo nixos-rebuild switch --flake . --option extra-substituters https://install.determinate.systems --option extra-trusted-public-keys cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM=
