9a930bc142a5669259a2ef7ea00611e034e19422
darrint <darrint@fastmail.com>
2025-12-01 17:24:22 -0500

    Update to 25.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     snowfall-lib = {
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager.url = "github:nix-community/home-manager?ref=release-25.05";
+    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     determinate = {
       url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
