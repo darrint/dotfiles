73712d430ce127478b5b212d4003506de6f60628
darrint <darrint@fastmail.com>
2026-04-17 11:13:36 -0400

    feat: make SSH authorized key the default in darrint-user module, applying to all hosts

--- modules/nixos/darrint-user/default.nix
+++ modules/nixos/darrint-user/default.nix
@@ -11,7 +11,9 @@ in
     };
     authorizedKeys = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [ ];
+      default = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCr1N+n9997wxRQ+Ss3oj1Ztg726vSVMelxdoSDkM/kqUL6ylELfyiF6ZYeZbTftd4TzJRW8zloltpVE1GnoaNnm/0clLCtJK6gQRNBKx+JBZaF26WpH0yo+Vt2puvajAchrfpzZl/HqZL5RscY+Gs8hS+u7IfbWQ8o/JhyUsY2rgsPSw58LQS8br22EZAcBCOMLXffer7l5489/g83sTKdgyvW2kWq+E97uMswJ2ytAhNbCdYDIGuceHymIVkNAmJ6FS3ykCiOthYGCNIWReR4VQRMix0wqxxxfXrtSXyYio3lhUTEnA2jWmJiVOfy94vRfkMFopuDOp2nOIxbKJhl"
+      ];
       description = "SSH authorized keys for the darrint user.";
     };
   };
