338941f9838484c0cbddbb2290ca3ae993b821a7
darrint <darrint@fastmail.com>
2026-04-23 21:09:51 -0400

    take over gh config

--- modules/home/darrint-git/default.nix
+++ modules/home/darrint-git/default.nix
@@ -18,6 +18,21 @@ in
       userEmail = "darrint@fastmail.com";
     };
     programs.lazygit.enable = true;
-    programs.gh.enable = true;
+    programs.gh = {
+      enable = true;
+      settings = {
+        version = 1;
+        git_protocol = "https";
+        prompt = "enabled";
+        prefer_editor_prompt = "disabled";
+        color_labels = "disabled";
+        accessible_colors = "disabled";
+        accessible_prompter = "disabled";
+        spinner = "enabled";
+        aliases = {
+          co = "pr checkout";
+        };
+      };
+    };
   };
 }
