1607cfd7d45032e17440f064fcd7ff11a3fbfccf
darrint <darrint@fastmail.com>
2026-05-12 21:37:45 -0400

    flake update and fix vim

--- modules/home/darrint-neovim/default.nix
+++ modules/home/darrint-neovim/default.nix
@@ -3,11 +3,9 @@
   lib,
   config,
   ...
-}:
-let
+}: let
   cfg = config.darrint.neovim;
-in
-{
+in {
   imports = [
     inputs.nvf.homeManagerModules.default
   ];
@@ -236,7 +234,6 @@ in
           obsidian.enable = false; # FIXME: neovim fails to build if obsidian is enabled
           neorg.enable = false;
           orgmode.enable = false;
-          mind-nvim.enable = true;
           todo-comments.enable = true;
         };
 
