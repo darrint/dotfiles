71924846434de429f8f32c1d144c7b5f7216ae11
darrint <darrint@fastmail.com>
2026-08-13 14:15:24 -0400

    Add `za` utility.

--- modules/home/darrint-zellij/default.nix
+++ modules/home/darrint-zellij/default.nix
@@ -1,12 +1,14 @@
 {
+  pkgs,
   lib,
   config,
   ...
-}:
-let
+}: let
   cfg = config.darrint.zellij;
-in
-{
+  za = pkgs.writeShellScriptBin "za" ''
+    exec ${pkgs.zellij}/bin/zellij attach "$@"
+  '';
+in {
   options.darrint.zellij = {
     enable = lib.mkEnableOption "Enable zellij and config";
   };
@@ -14,5 +16,26 @@ in
   config = lib.mkIf cfg.enable {
     programs.zellij.enable = true;
     home.file.".config/zellij/config.kdl".source = ./zellij.kdl;
+    home.packages = [za];
+
+    programs.bash.initExtra = ''
+      _za() {
+        local sessions
+        sessions="$(${pkgs.zellij}/bin/zellij list-sessions -n -s 2>/dev/null)"
+        COMPREPLY=($(compgen -W "$sessions" -- "''${COMP_WORDS[COMP_CWORD]}"))
+      }
+      complete -F _za za
+    '';
+    programs.zsh.initContent = ''
+      _za() {
+        local -a sessions
+        sessions=(''${(f)"$(${pkgs.zellij}/bin/zellij list-sessions -n -s 2>/dev/null)"})
+        _describe 'session' sessions
+      }
+      compdef _za za
+    '';
+    programs.fish.shellInit = ''
+      complete -c za -f -a "(${pkgs.zellij}/bin/zellij list-sessions -n -s 2>/dev/null)"
+    '';
   };
 }
